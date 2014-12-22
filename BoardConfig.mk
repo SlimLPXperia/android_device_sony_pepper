61866b1f57aaa8b6caacd5757c4b27d57cf1bc4a
Adeel Javed <alwaysadeel@gmail.com>
2014-12-22 11:51:27 +0500

    f2fs defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,7 +11,7 @@ TARGET_SPECIFIC_HEADER_PATH += device/sony/pepper/include
 
 
 # Kernel specific configurations
-TARGET_KERNEL_CONFIG := xperiasola_defconfig
+TARGET_KERNEL_CONFIG := pepper_defconfig
 
 # TWRP
 DEVICE_RESOLUTION := 480x854
